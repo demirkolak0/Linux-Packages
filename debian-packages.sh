d50e8e3311fba45450ee8a4ea2c2075b962291e0
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2024-01-12 14:19:15 +0300

    Create debian-packages.sh

---
+++ debian-packages.sh
@@ -0,0 +1,1 @@
+sudo apt install micro vim iw aircrack-ng iproute2 procps gawk xterm tmux pciutils hashcat mdk3 mdk4 hostapd lighttpd iptables bettercap hcxdumptool tshark isc-dhcp-server dnsmasq openssl
