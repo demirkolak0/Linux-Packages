08424d82a2d21d897df14cac8e993cd7e0916dec
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2024-06-16 12:44:37 +0300

    Update grub-setup.sh

--- grub-setup.sh
+++ grub-setup.sh
@@ -1,1 +1,1 @@
-pacman -S grub efibootmgr && mkdir /boot/efi && mount /dev/sda1 /boot/efi && grub-install --target=x86_64-efi --bootloader-id=Arch Linux EFI --efi-directory=/boot/efi && grub-mkconfig -o /boot/grub/grub.cfg
+pacman -S grub efibootmgr && grub-install --target=x86_64-efi --bootloader-id=Arch Linux EFI --efi-directory=/boot/efi && grub-mkconfig -o /boot/grub/grub.cfg
