2f164773ae50e44dc2eaa91639693a74982eca17
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2024-07-18 02:48:17 +0300

    Create fedora40-firstboot.sh

---
+++ fedora40-firstboot.sh
@@ -0,0 +1,16 @@
+sudo dnf update -y
+sudo dnf install java-21-openjdk nodejs wget librsvg2-devel librsvg2-tools mpv-libs-devel qt5-qtbase-devel qt5-qtwebengine-devel qt5-qtquickcontrols qt5-qtquickcontrols2 openssl-devel gcc g++ make glibc-devel kernel-headers binutils google-chrome-stable neofetch htop gnome-tweaks vim -y
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+sudo dnf check-update
+sudo dnf install code -y
+git clone --recurse-submodules https://github.com/Stremio/stremio-shell.git
+cd stremio-shell
+sed -i 's/qmake/qmake-qt5/g' release.makefile
+qmake-qt5
+make -f release.makefile
+sudo make -f release.makefile install
+sudo ./dist-utils/common/postinstall
+cd ..
+rm -rf stremio-shell
+echo "Done!!"
