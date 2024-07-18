b4e34c725838404984fd8924a95af868fe9bbfc5
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2024-07-18 03:00:21 +0300

    Update fedora40-firstboot.sh

--- fedora40-firstboot.sh
+++ fedora40-firstboot.sh
@@ -1,5 +1,8 @@
 sudo dnf update -y
 sudo dnf install java-21-openjdk nodejs wget librsvg2-devel librsvg2-tools mpv-libs-devel qt5-qtbase-devel qt5-qtwebengine-devel qt5-qtquickcontrols qt5-qtquickcontrols2 openssl-devel gcc g++ make glibc-devel kernel-headers binutils google-chrome-stable neofetch htop gnome-tweaks vim -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf update -y
+sudo dnf install discord -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 sudo dnf check-update
