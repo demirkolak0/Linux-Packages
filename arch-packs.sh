0d138d1f514dfe6d12e54416805bfb7640a10461
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2025-01-09 15:14:59 +0300

    Delete arch-packs.sh

--- arch-packs.sh
+++
@@ -1,1 +0,0 @@
-pacstrap /mnt base base-devel linux-lts linux-headers linux-firmware nano vim gcc networkmanager network-manager-applet usbutils grub efibootmgr mtools gvfs ntfs-3g pacman-contrib reflector git wget yajl dosfstools mtools s-nail mdadm bash-completion numlockx man-db man-pages dialog wpa_supplicant wireless_tools netctl inetutils dhcpcd neofetch htop os-prober iwd
