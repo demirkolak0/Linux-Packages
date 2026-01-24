aaa0b8123582766e3e6347ee11b15a738bbf6f7d
Demir Kolak <112659238+demirkolak0@users.noreply.github.com>
2026-01-25 00:42:24 +0300

    fix

--- alpine-unrar-package/alpine-unrar-install.sh
+++ alpine-unrar-package/alpine-unrar-install.sh
@@ -1,7 +1,7 @@
 echo "**** install unrar from source ****"
 cd /tmp
 curl -o unrar.tar.gz https://www.rarlab.com/rar/unrarsrc-7.2.3.tar.gz
-apk add build-dependencies build-base libffi-dev openssl-dev python3-dev
+apk add build-base libffi-dev openssl-dev python3-dev
 apk add 7zip par2cmdline python3
 mkdir /tmp/unrar
 tar xf /tmp/unrar.tar.gz -C /tmp/unrar --strip-components=1
