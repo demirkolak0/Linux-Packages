c5ae5956631b83a745dbfdf99c51aafec4707486
Demir Kolak <demirkolak8@gmail.com>
2026-04-13 14:23:05 +0300

    Update fedora42-post-install.sh

--- fedora42-post-install.sh
+++ fedora42-post-install.sh
@@ -1,8 +1,8 @@
 sudo dnf update -y
 sudo dnf install java-21-openjdk nodejs wget librsvg2-devel librsvg2-tools mpv-libs-devel qt5-qtbase-devel qt5-qtwebengine-devel qt5-qtquickcontrols qt5-qtquickcontrols2 openssl-devel gcc g++ make glibc-devel kernel-headers binutils fastfetch htop google-chrome-stable gnome-tweaks vim libappindicator-gtk3 gnome-shell-extension-appindicator gnome-extensions-app -y
 sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 sudo dnf update -y
-sudo dnf install discord code -y
+sudo dnf install sublime-text discord  -y
 echo "Done!!"
