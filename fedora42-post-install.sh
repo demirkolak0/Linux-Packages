e290f9e7385742577263cf5bd34704760d91bf58
Demir Kolak <demirkolak8@gmail.com>
2026-04-30 23:39:53 +0300

    Update and rename fedora42-post-install.sh to fedora44-post-install.sh

--- fedora42-post-install.sh
+++
@@ -1,8 +0,0 @@
-sudo dnf update -y
-sudo dnf install java-21-openjdk nodejs wget librsvg2-devel librsvg2-tools mpv-libs-devel qt5-qtbase-devel qt5-qtwebengine-devel qt5-qtquickcontrols qt5-qtquickcontrols2 openssl-devel gcc g++ make glibc-devel kernel-headers binutils fastfetch htop google-chrome-stable gnome-tweaks vim libappindicator-gtk3 gnome-shell-extension-appindicator gnome-extensions-app -y
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-sudo dnf update -y
-sudo dnf install sublime-text discord  -y
-echo "Done!!"
