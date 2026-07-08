cb9cf514b5338a4b3adc4d3f5db08c2ddc72f045
Demir Kolak <demirkolak8@gmail.com>
2026-07-08 03:17:59 +0300

    Update stremio-build-fedora.sh

--- stremio-build-fedora.sh
+++ stremio-build-fedora.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
+set -e
 git clone --recurse-submodules https://github.com/Stremio/stremio-shell.git
 cd stremio-shell
 sed -i 's/qmake/qmake-qt5/g' release.makefile
 qmake-qt5
 make -f release.makefile
-sudo make -f release.makefile install
-sudo ./dist-utils/common/postinstall
+./dist-utils/common/postinstall
 cd ..
 rm -rf stremio-shell
